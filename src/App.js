dfc59ebcf536ef92311330896dc4c7406f0374a5
Gagik_Khalatyan <Gagik_Khalatyan@EPAMYERW008B>
2023-09-05 13:06:23 +0400

    delete files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchForm from './SearchForm/SearchForm';
-import CounterForm from './Counter/CounterForm';
-import GenreSelect from './GenreSelect/GenreSelect';
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div className="App">
-     <CounterForm/>
-     <SearchForm/>
-     <GenreSelect/>
+   
     </div>
   );
 }
