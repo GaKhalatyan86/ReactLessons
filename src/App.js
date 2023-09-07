9b3254e622e225f911efa1a6c496497186e9e7d0
Gagik_Khalatyan <Gagik_Khalatyan@EPAMYERW008B>
2023-09-07 13:58:42 +0400

    new change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SearchForm from './SearchForm/SearchForm';
+import CounterForm from './Counter/CounterForm';
+import GenreSelect from './GenreSelect/GenreSelect';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-   
+     <CounterForm/>
+     <SearchForm/>
+     <GenreSelect/>
     </div>
   );
 }
---
+++ src/Counter/ButtonList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ButtonList(onClickButton)
+{
+        return <div>
+                   <button  className="btn btn-primary" name="inc" onClick={()=>onClickButton.onClickHandler(1)}>Increment</button>
+                   <button  name="dec" className="btn btn-primary" onClick={()=>onClickButton.onClickHandler(-1)}>Decrement</button>
+               </div>
+}
+export default ButtonList
