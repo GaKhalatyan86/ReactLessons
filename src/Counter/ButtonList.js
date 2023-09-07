8a4763420ca4ece210770b8916061fa2eb1ec556
Gagik_Khalatyan <Gagik_Khalatyan@EPAMYERW008B>
2023-09-07 14:58:20 +0400

    code formting

--- src/Counter/ButtonList.js
+++ src/Counter/ButtonList.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-function ButtonList(onClickButton)
-{
-        return <div>
-                   <button  className="btn btn-primary" name="inc" onClick={()=>onClickButton.onClickHandler(1)}>Increment</button>
-                   <button  name="dec" className="btn btn-primary" onClick={()=>onClickButton.onClickHandler(-1)}>Decrement</button>
-               </div>
+function ButtonList(onClickButton) {
+    return <div>
+        <button className="btn btn-primary" name="inc" onClick={() => onClickButton.onClickHandler(1)}>Increment</button>
+        <button name="dec" className="btn btn-primary" onClick={() => onClickButton.onClickHandler(-1)}>Decrement</button>
+    </div>
 }
 export default ButtonList
--- src/Counter/CounterForm.js
+++ src/Counter/CounterForm.js
@@ -1,28 +1,25 @@
-import React, { useState } from  "react";
+import React, { useState } from "react";
 import ButtonList from "./ButtonList";
 
-class CounterForm extends React.Component 
-{
-   constructor(props)
-   {
-       super(props);
+class CounterForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-       this.state={
-        counter:0
-       };
-   }
-   handler=(val)=>
-   {
-     this.setState({counter:(this.state.counter+val)})
-   }
-   
-   render(){  
-       return <div>
-                 <h2>{this.state.counter}</h2>
-                <ButtonList onClickHandler={this.handler}/>
-              </div>;
-   }
-   
+    this.state = {
+      counter: 0
+    };
+  }
+  handler = (val) => {
+    this.setState({ counter: (this.state.counter + val) })
+  }
+
+  render() {
+    return <div>
+      <h2>{this.state.counter}</h2>
+      <ButtonList onClickHandler={this.handler} />
+    </div>;
+  }
+
 }
 
 export default CounterForm
